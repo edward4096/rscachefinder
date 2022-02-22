13f7e4e8f989f7ad15b81172def4d4e19df6f9ea
edward4096 <100126327+edward4096@users.noreply.github.com>
2022-02-22 13:27:36 +0000

    Update win95_edit.cpp

--- win95_edit.cpp
+++ win95_edit.cpp
@@ -2,10 +2,14 @@
 #include <stdio.h>
 int main(int argc,char**argv) {
   FILE*f=fopen(argv[1],"r+b");
+  int off;
+  fseek(f,0x03C,SEEK_SET);
+  fread(&off,1,4,f);
+  printf("off=%d\n", off);
   char version=4;
-  fseek(f,0x110,SEEK_SET);
+  fseek(f,0x40+off,SEEK_SET);
   fwrite(&version,1,1,f);
-  fseek(f,0x118,SEEK_SET);
+  fseek(f,0x48+off,SEEK_SET);
   fwrite(&version,1,1,f);
   return 0;
 }
